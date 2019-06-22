3db47354894f8c0b5c7abcc4fbd593b26ed92383
andrewronscki <andrewronscki@gmail.com>
2019-06-21 23:13:56 -0300

    Commit full

---
+++ src/components/CourseList/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+function addCourseAction(title) {
+  return { type: "ADD_COURSE", title };
+}
+
+export default function CourseList() {
+  const qty = 4;
+
+  const courses = useSelector(state => state.data.slice(0, qty), [qty]);
+  const dispatch = useDispatch();
+
+  function addCourse() {
+    dispatch(addCourseAction("GraphQL"));
+  }
+
+  return (
+    <>
+      <ul>
+        {courses.map(course => (
+          <li key={course}>{course}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={addCourse}>
+        Adicionar curso
+      </button>
+    </>
+  );
+}
